e202b041c32dbd92a0d968e9eeea73de5e29d93d
Nihanawesomefish <80280683+Nihanawesomefish@users.noreply.github.com>
2021-11-28 20:20:12 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,6 +1,14 @@
 
 //ADD YOUR FIREBASE LINKS HERE
-
+var firebaseConfig = {
+      apiKey: "AIzaSyAo4AlBskAXrPJ5XW33HfCy2nQFKQab9k4",
+      authDomain: "social-d420f.firebaseapp.com",
+      databaseURL: "https://social-d420f-default-rtdb.firebaseio.com",
+      projectId: "social-d420f",
+      storageBucket: "social-d420f.appspot.com",
+      messagingSenderId: "503299505254",
+      appId: "1:503299505254:web:041e83dc5f611be62081a7"
+    };
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
@@ -8,3 +16,16 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //End code
       });});}
 getData();
+function logout(){
+      user_name=document.getElementById("user_name").value;
+      localStorage.setItem("user_name", user_name);
+      window.location="index.html";
+      }
+      function addRoom(){
+            user_name=document.getElementById("room_name").value;
+            firebase.database().ref("/").childKey(room_name).update({
+            purpose:"Nihan is adding the room"
+            });
+              }
+              var app = initializeApp(firebaseConfig);
+      
