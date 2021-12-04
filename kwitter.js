18c1f90e762c198058f780520638776bff679f79
Nihanawesomefish <80280683+Nihanawesomefish@users.noreply.github.com>
2021-12-03 22:39:53 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -3,14 +3,3 @@ user_name=document.getElementById("user_name").value;
 localStorage.setItem("user_name", user_name);
 window.location="kwitter_room.html";
 }
-var firebaseConfig = {
-    apiKey: "AIzaSyAo4AlBskAXrPJ5XW33HfCy2nQFKQab9k4",
-    authDomain: "social-d420f.firebaseapp.com",
-    databaseURL: "https://social-d420f-default-rtdb.firebaseio.com",
-    projectId: "social-d420f",
-    storageBucket: "social-d420f.appspot.com",
-    messagingSenderId: "503299505254",
-    appId: "1:503299505254:web:041e83dc5f611be62081a7"
-  };
-  firebase.initializeApp(firebaseConfig);
-  
--- kwitter_room.js
+++ kwitter_room.js
@@ -9,10 +9,15 @@ var firebaseConfig = {
       messagingSenderId: "503299505254",
       appId: "1:503299505254:web:041e83dc5f611be62081a7"
     };
+   firebase.initializeApp(firebaseConfig);
+   user_name=localStorage.getItem("user_name");
+   document.getElementById("user_name").innerHTML="Welcome "+user_name+" ツ";
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+console.log("room_name"+Room_names);
+row="<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+document.getElementById("output").innerHTML+=row;
       //End code
       });});}
 getData();
@@ -22,10 +27,16 @@ function logout(){
       window.location="index.html";
       }
       function addRoom(){
-            user_name=document.getElementById("room_name").value;
-            firebase.database().ref("/").childKey(room_name).update({
+            room_name=document.getElementById("room_name").value;
+            firebase.database().ref("/").child(room_name).update({
             purpose:"Nihan is adding the room"
             });
+            localStorage.setItem("room_name",room_name);
+            window.location="kwitter_page.html"
               }
-              var app = initializeApp(firebaseConfig);
+            function redirectToRoomName(name){
+            console.log(name);
+            localStorage.setItem("room_name",room_name);
+            window.location="kwitter_page.html"
+            }
       
