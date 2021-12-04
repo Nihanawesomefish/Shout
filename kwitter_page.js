a804b73ea9910c1ecf5cf1590fd48ec4d628a3e1
Nihanawesomefish <80280683+Nihanawesomefish@users.noreply.github.com>
2021-12-04 13:08:34 -0500

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -37,3 +37,4 @@ like:0
 });
 document.getElementById("msg").value="";
 }
+function updateLike(message_id) { console.log("clicked on like button - " + message_id); button_id = message_id; likes = document.getElementById(button_id).value; updated_likes = Number(likes) + 1; console.log(updated_likes); firebase.database().ref(room_name).child(message_id).update({ like : updated_likes }); }
